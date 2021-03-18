637ba596adf2b4c0e7dcddeec10827da92789fda
user <user@ws1013402136.pm>
2021-03-18 14:15:30 +0500

    lab1

---
+++ lab2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+  const int a = 10;
+    double x;
+    printf("Enter x");
+    scanf("%lf", &x);
+    double y = (pow(a,x)+pow(a,-x))/2;
+    if ((y>=-1) && (y<=2))
+    {
+      double z = sqrt (2+y-y*y);
+      printf("z=%lf", z);
+    }
+    else
+      printf("Error");
+    return 0;
+}
