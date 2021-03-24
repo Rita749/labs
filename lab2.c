d23982c5ac650040150ccd72f21f9dc37becb518
Rita749 <80448319+Rita749@users.noreply.github.com>
2021-03-24 21:19:29 +0500

    Update lab2.c

--- lab2.c
+++ lab2.c
@@ -2,17 +2,18 @@
 #include <math.h>
 int main()
 {
-  const int a = 10;
-    double x;
-    printf("Enter x");
+    const int a = 2;
+    double x, z;
+    printf("Enter x: ");
     scanf("%lf", &x);
-    double y = (pow(a,x)+pow(a,-x))/2;
-    if ((y>=-1) && (y<=2))
+    double y = (pow(a, x) + pow(a, -x)) / 2;
+    printf("y(x) = %lf\n", y);
+    if ((y >= -1) && (y <= 2))
     {
-      double z = sqrt(2+y-y*y);
-      printf("z=%lf", z);
+        double z = sqrt(2 + y - y * y);
+        printf("z(y) = %lf", z);
     }
     else
-      printf("Error");
+        printf("Y value is incorrect!");
     return 0;
 }
