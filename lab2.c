5a9faeb356c332af096f7c67f4c6c0fc1d5f3133
user <user@ws1013402136.pm>
2021-03-25 13:32:31 +0500

    lab2

--- lab2.c
+++ lab2.c
@@ -6,14 +6,15 @@ int main()
     double x, z;
     printf("Enter x: ");
     scanf("%lf", &x);
-    double y = (pow(a, x) + pow(a, -x)) / 2;
-    printf("y(x) = %lf\n", y);
-    if ((y >= -1) && (y <= 2))
+    if ((x > -1.9) && (x < 1.9))
     {
+        double y = (pow(a, x) + pow(a, -x)) / 2;
+        printf("y(x) = %lf\n", y);
+    //if ((y >= -1) && (y <= 2))
         double z = sqrt(2 + y - y * y);
         printf("z(y) = %lf", z);
     }
     else
-        printf("Y value is incorrect!");
+        printf("X value is incorrect!");
     return 0;
 }
